02d4cebdff60c488473920ecadf22a90524a7cf0
golden-fish-nthu <josh101.tw@gapp.nthu.tw>
2024-09-27 21:11:29 +0800

    goog

--- SUS.c
+++ SUS.c
@@ -1,40 +1,16 @@
+#include <math.h>
 #include <stdio.h>
-#include <stdlib.h>
 
-int countTreesUtil(int pre[], int post[], int* preIndex, int l, int h, int size) {
-    // Base case
-    if (*preIndex >= size || l > h)
-        return 1;
-
-    // The first node in preorder traversal is root. So take the node at
-    // preIndex from preorder and make it root, and increment preIndex
-    int root = pre[*preIndex];
-    ++*preIndex;
-
-    // If the current subarray has only one element, no need to recur
-    if (l == h)
-        return 1;
-
-    // Search the next element of pre[] in post[]
-    int i;
-    for (i = l; i <= h; ++i)
-        if (pre[*preIndex] == post[i])
-            break;
-
-    // Use the index of element found in postorder to divide
-    // postorder array in two parts. Left subtree and right subtree
-    int leftCount = 0, rightCount = 0;
-    if (i <= h) {
-        leftCount = countTreesUtil(pre, post, preIndex, l, i, size);
-        rightCount = countTreesUtil(pre, post, preIndex, i + 1, h - 1, size);
+// Function to count nodes with only one child
+int countOneChildNodes(int pre[], int post[], int n) {
+    int count = 0;
+    for (int i = 0; i < n - 1; i++) {
+        if (pre[i + 1] != post[i]) {
+            printf("Node with one child: %d\n", pre[i + 1]);
+            count++;
+        }
     }
-
-    return leftCount * rightCount;
-}
-
-int countTrees(int pre[], int post[], int size) {
-    int preIndex = 0;
-    return countTreesUtil(pre, post, &preIndex, 0, size - 1, size);
+    return count;
 }
 
 int main() {
@@ -48,10 +24,9 @@ int main() {
         for (int i = 0; i < n; i++) {
             scanf("%d", &post[i]);
         }
+        int result = pow(2, countOneChildNodes(pre, post, n));
 
-        int result = countTrees(pre, post, n);
         printf("%d\n", result);
     }
-
     return 0;
 }
