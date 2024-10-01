dcb251a13d9bfa80d13dd6f5a781a4462fba1c8e
golden-fish-nthu <josh101.tw@gmail.com>
2024-10-01 22:52:39 +0800

    HUH

--- count.c
+++ count.c
@@ -1,26 +1,28 @@
+#include <math.h>
 #include <stdio.h>
-#define ll long long
-int fun2(int i) {
-  int ret = 0;
-  while (i) {
-    ret += ((i % 10) == 1);
-    i /= 10;
-  }
-  return ret;
-}
-int pre[1000001];
-int main() {
-  int a, b;
-  int ti;
-  scanf("%d", &ti);
-  for (int i = 0; i < 1000001; i++) {
-    int ones = fun2(i);
-    pre[i] = ones + pre[i - 1];
-  }
-  for (int i = 0; i < ti; i++) {
-    scanf("%d %d", &a, &b);
-    printf("%d\n", pre[b] - pre[a - 1]);
-  }
 
-  return 0;
+int main() {
+    int a, b, c;
+    printf("Enter three integers: ");
+    scanf("%d %d %d", &a, &b, &c);
+
+    // (2) 若a大於等於60且小於100則輸出1，否則輸出0。
+    if (a >= 60 && a < 100) {
+        printf("1\n");
+    } else {
+        printf("0\n");
+    }
+
+    // (3) 計算b+1再除以10的值，四捨五入至小數點後第二位。
+    double result = (b + 1) / 10.0;
+    printf("%.2f\n", round(result * 100) / 100);
+
+    // (4) 若a大於等於c，則輸出a，否則輸出c。
+    if (a >= c) {
+        printf("%d\n", a);
+    } else {
+        printf("%d\n", c);
+    }
+
+    return 0;
 }
